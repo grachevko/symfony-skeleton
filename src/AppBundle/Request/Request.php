b0c760d7a3f777fe11829fc8f74c56120f37ce26
Konstantin Grachev <ko@grachev.io>
2016-08-08 23:36:03 +0300

    Add original request to custom

--- src/AppBundle/Request/Request.php
+++ src/AppBundle/Request/Request.php
@@ -6,6 +6,8 @@ use AppBundle\Traits\PropertyAssignTrait;
 use AppBundle\Traits\PropertyGetterTrait;
 
 /**
+ * @property \Symfony\Component\HttpFoundation\Request $request
+ *
  * @author Konstantin Grachev <ko@grachev.io>
  */
 abstract class Request
@@ -16,10 +18,17 @@ abstract class Request
     protected static $defaults = [];
 
     /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
+
+    /**
      * @param \Symfony\Component\HttpFoundation\Request $request
      */
     public function handleRequest(\Symfony\Component\HttpFoundation\Request $request)
     {
+        $this->request = $request;
+
         $this->assign(array_merge(static::$defaults, $request->query->all()));
     }
 }
