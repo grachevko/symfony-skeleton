55d3f312e8658e5862905a4dabecd0b297934f3c
Konstantin Grachev <ko@grachev.io>
2016-09-25 20:50:19 +0300

    Add createQueryBuilder and paginate methods

--- src/AppBundle/Doctrine/EntityRepository.php
+++ src/AppBundle/Doctrine/EntityRepository.php
@@ -4,6 +4,9 @@ namespace AppBundle\Doctrine;
 
 use AppBundle\Exception\InvalidArgumentException;
 use Doctrine\ORM\EntityNotFoundException;
+use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
 use Ramsey\Uuid\UuidInterface;
 
 /**
@@ -61,5 +64,31 @@ abstract class EntityRepository
 
         $this->em->persist($entity);
         $this->em->flush($entity);
+    }
+
+    /**
+     * @param $alias
+     *
+     * @return QueryBuilder
+     */
+    public function createQueryBuilder($alias): QueryBuilder
+    {
+        return $this->em->createQueryBuilder()
+            ->select($alias)
+            ->from($this->getClass(), $alias);
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     * @param int          $pageSize
+     * @param int          $pageIndex
+     *
+     * @return Pagerfanta
+     */
+    protected function paginate(QueryBuilder $qb, int $pageSize, int $pageIndex): Pagerfanta
+    {
+        return (new Pagerfanta(new DoctrineORMAdapter($qb)))
+            ->setMaxPerPage($pageSize)
+            ->setCurrentPage($pageIndex);
     }
 }
