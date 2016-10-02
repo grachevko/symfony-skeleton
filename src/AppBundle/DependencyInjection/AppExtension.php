fd27f213dbaf97ee2a8700d2b363967d646c61c3
Konstantin Grachev <ko@grachev.io>
2016-10-03 00:11:02 +0300

    Dynamic load config yml files

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -5,6 +5,7 @@ namespace AppBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -17,12 +18,13 @@ class AppExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('controllers.yml');
-        $loader->load('listeners.yml');
-        $loader->load('normalizers.yml');
-        $loader->load('repositories.yml');
-        $loader->load('request.yml');
-        $loader->load('services.yml');
+        $dir = __DIR__.'/../Resources/config';
+        $loader = new Loader\YamlFileLoader($container, new FileLocator($dir));
+
+        $finder = new Finder();
+        $finder->files()->name('*.yml');
+        foreach ($finder->in($dir) as $file) {
+            $loader->load($file->getBasename());
+        }
     }
 }
