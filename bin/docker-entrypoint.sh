8f5cbd56fbb5f240534cee0663cbd2e9d167096b
Konstantin Grachev <me@grachevko.ru>
2016-11-05 21:23:39 +0300

    Remove app_dev.php. Create kernel by environment variables.

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,83 +2,57 @@
 
 set -e
 
-SYMFONY_ENV=${SYMFONY_ENV:=dev}
-
-OPCACHE=
-COMPOSER=
-REQUIREMENTS=
-MIGRATION=
-FIXTURES=
-XDEBUG=
-COMMAND=
-
-for i in "$@"
-do
-case ${i} in
-    --no-composer)
-    COMPOSER=false
-    ;;
-    -m|--migrations)
-    MIGRATION=true
-    ;;
-    -f|--fixtures)
-    FIXTURES=true
-    ;;
-    --no-fixtures)
-    FIXTURES=false
-    ;;
-    -x|--xdebug)
-    XDEBUG=true
-    ;;
-    --no-xdebug)
-    XDEBUG=false
-    ;;
-    *)
-    # unknown option
-    COMMAND=${COMMAND}' '${i}
-    ;;
+case $SYMFONY_ENV in
+   prod|dev|test)
+	;;
+   *)
+	>&2 echo env "SYMFONY_ENV" must be in \"prod, dev, test\"
+	exit 1
+	;;
 esac
-    # past argument=value
-    shift
-done
+
+case $SYMFONY_DEBUG in
+   true|false)
+	;;
+   *)
+	>&2 echo env "SYMFONY_DEBUG" must in \"true, false\"
+	exit 1
+	;;
+esac
+
+if [ "$SYMFONY_ENV" == "dev" ]; then
+    COMPOSER_EXEC=${COMPOSER_EXEC:="composer install --no-interaction --optimize-autoloader --prefer-source"}
+    XDEBUG=${XDEBUG:=true}
+
+    COMMAND=${COMMAND:='bin/console server:run 0.0.0.0:80 --router="vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/config/router_prod.php"'}
+
+elif [ "$SYMFONY_ENV" == "prod" ]; then
+    COMPOSER_EXEC=${COMPOSER_EXEC:="composer install --no-dev --no-interaction --optimize-autoloader --no-progress --prefer-dist"}
+
+    rm -rf ${APP_DIR}/web/config.php
+    cp ${APP_DIR}/app/config/apache.conf ${APACHE_CONFDIR}/sites-enabled/000-default.conf
+
+    a2enmod rewrite
+    COMMAND=${COMMAND:=apache2-foreground}
+
+elif [ "$SYMFONY_ENV" == "test" ]; then
+	COMPOSER_EXEC=${COMPOSER_EXEC:="composer install --no-interaction --optimize-autoloader --no-progress --prefer-dist"}
+	REQUIREMENTS=${REQUIREMENTS:=true}
+	FIXTURES=${FIXTURES:=true}
+
+	COMMAND=${COMMAND:="php-cs-fixer fix --dry-run --level symfony ./src/ && bin/console doctrine:schema:validate && phpunit"}
+fi
+
+OPCACHE=${OPCACHE:=true}
+MIGRATION=${MIGRATION:=true}
 
 {
     echo 'date.timezone = UTC';
     echo 'short_open_tag = off';
 } > ${PHP_INI_DIR}/php.ini
 
-if [ "$SYMFONY_ENV" == "dev" ]; then
-    XDEBUG=${XDEBUG:=true}
-    COMPOSER=${COMPOSER:="composer install --no-interaction --optimize-autoloader --prefer-source"}
-
-    COMMAND=${COMMAND:='bin/console server:run 0.0.0.0:80'}
-fi
-
-if [ "$SYMFONY_ENV" == "test" ]; then
-    export SYMFONY_DEBUG=0
-
-    OPCACHE=${OPCACHE:=true}
-    COMPOSER=${COMPOSER:="composer install --no-interaction --optimize-autoloader --no-progress --prefer-dist"}
-    REQUIREMENTS=${REQUIREMENTS:=true}
-    MIGRATION=${MIGRATION:=true}
-    FIXTURES=${FIXTURES:=true}
-
-    COMMAND=${COMMAND:="bin/console doctrine:schema:validate && phpunit"}
-fi
-
-if [ "$SYMFONY_ENV" == "prod" ]; then
-    OPCACHE=${OPCACHE:=true}
-    COMPOSER=${COMPOSER:="composer install --no-dev --no-interaction --optimize-autoloader --no-progress --prefer-dist"}
-    MIGRATION=${MIGRATION:=true}
-
-#    COMMAND=${COMMAND:="php-fpm"}
-    COMMAND=${COMMAND:="apache2-foreground"}
-fi
 
 if [ "$OPCACHE" == "true" ]; then
-#    docker-php-ext-enable opcache # wait for fix "nm not found"
-    echo "zend_extension=opcache.so" > ${PHP_INI_DIR}/conf.d/docker-php-ext-opcache.ini
-
     {
         echo 'opcache.enable = 1';
         echo 'opcache.enable_cli = 1';
@@ -94,11 +68,12 @@ if [ "$OPCACHE" == "true" ]; then
         echo 'opcache.load_comments = 1';
     } > ${PHP_INI_DIR}/conf.d/opcache.ini
 
+    docker-php-ext-enable opcache
     echo -e '\n > opcache enabled\n'
 fi
 
-if [ "$COMPOSER" != "false" ]; then
-    ${COMPOSER}
+if [ "$COMPOSER_EXEC" != "false" ]; then
+    ${COMPOSER_EXEC}
 fi
 
 if [ "$REQUIREMENTS" == "true" ]; then
@@ -106,7 +81,7 @@ if [ "$REQUIREMENTS" == "true" ]; then
 fi
 
 if [ "$MIGRATION" == "true" ]; then
-    bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+    bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration --quiet
 fi
 
 if [ "$FIXTURES" == "true" ]; then
@@ -114,9 +89,6 @@ if [ "$FIXTURES" == "true" ]; then
 fi
 
 if [ "$XDEBUG" == "true" ]; then
-#    docker-php-ext-enable xdebug # wait for fix "nm not found"
-    echo "zend_extension=xdebug.so" > ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
-
     {
         echo 'xdebug.remote_enable=On';
         echo 'xdebug.remote_autostart=On';
@@ -125,12 +97,10 @@ if [ "$XDEBUG" == "true" ]; then
         echo 'xdebug.file_link_format="phpstorm://open?file=%f&line=%l"';
     } > ${PHP_INI_DIR}/conf.d/xdebug.ini
 
+    docker-php-ext-enable xdebug
     echo -e '\n> xdebug enabled\n'
 fi
 
-if [ "$SYMFONY_ENV" == "prod" ]; then
-    chown -R www-data:www-data ${APP_DIR}/var
-    rm -rf ${APP_DIR}/bin/sf ${APP_DIR}/web/config.php ${APP_DIR}/web/app_dev.php
-fi
+chown -R www-data:www-data ${APP_DIR}/var
 
 /bin/sh -c "${COMMAND}"
